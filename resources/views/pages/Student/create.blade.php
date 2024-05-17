4a101445827e964df20788642f534315df145f04
NabinGhimire0 <nabinghimire002@gmail.com>
2024-05-17 10:07:00 +0545

    env

--- resources/views/pages/Student/create.blade.php
+++ resources/views/pages/Student/create.blade.php
@@ -5,20 +5,20 @@
 @section('content')
 <form>
     <div class="mb-3 container-sm">
-      <label for="exampleInputEmail1" class="form-label">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-      <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+      <label for="name" class="form-label">Name</label>
+      <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="name">
     </div>
     <div class="mb-3 container-sm">
-      <label for="exampleInputPassword1" class="form-label">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1">
+      <label for="roll_no" class="form-label">Roll No</label>
+      <input type="text" class="form-control" id="exampleInputPassword1" name="roll_no">
     </div>
-    <div class="mb-3 container-sm form-check">
-      <input type="checkbox" class="form-check-input" id="exampleCheck1">
-      <label class="form-check-label" for="exampleCheck1">Check me out</label>
-    </div>
-    <div class="container-sm">
 
+    <div class="mb-3 container-sm">
+        <label for="roll_no" class="form-label">Faculty</label>
+        <input type="text" class="form-control" id="exampleInputPassword1" name="faculty">
+    </div>
+
+    <div class="container-sm">
         <button type="submit" class="btn btn-primary">Submit</button>
     </div>
   </form>
---
+++ resources/views/pages/Student/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+@section('title')
+ student
+@endsection
+@section('content')
+<h1>Students Detail</h1>
+@endsection
